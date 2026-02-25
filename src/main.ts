536caefc553325949be6c346fa495c4a6e207455
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2026-02-25 17:35:25 +0100

    config, versioning and prefix config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { RequestMethod, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configServer = app.get(ConfigService);
+  const port = configServer.get<number>('PORT') || 3001;
+
+  app.setGlobalPrefix('api', {
+    exclude: [
+      {
+        path: '/',
+        method: RequestMethod.GET,
+      },
+    ],
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1.0',
+    prefix: 'v',
+  });
+
+  await app.listen(port);
 }
 bootstrap();
